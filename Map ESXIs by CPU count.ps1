596e5facafbc3e32039463bc4894800bf4045210
omrikas <65962803+omrikas@users.noreply.github.com>
2020-05-26 17:20:20 +0300

    Add files via upload

---
+++ Map ESXIs by CPU count.ps1
@@ -0,0 +1,67 @@
+﻿Import-Module VMware.VimAutomation.Core
+
+# Create the excel with the filled cells
+function CreateExcel()
+{
+    # Write cells text
+    $WorkSheet.Cells.Item(1, 1).value() = 'VCenter'    
+    $WorkSheet.Cells.Item(1, 2).value() = 'Cluster' 
+    $WorkSheet.Cells.Item(1, 3).value() = 'Name' 
+    $WorkSheet.Cells.Item(1, 4).value() = 'CPU' 
+
+    # Set the cells as bold and text in center
+    for ($i = 1; $i -le 4; $i = $i + 1)
+    {
+        if ($i -lt 6)
+        { $MergeCells = $WorkSheet.range($WorkSheet.cells.item(1,$i), $WorkSheet.cells.item(2,$i))
+          $MergeCells.MergeCells = $true
+          $mergeCells.BorderAround(1,3,1) *> $null }
+        $WorkSheet.Cells.item(1, $i).font.bold = $true  
+        $WorkSheet.Cells.item(1, $i).horizontalalignment = -4108
+        $WorkSheet.Cells.item(1, $i).verticalalignment = -4108
+    }
+
+    # Auto fit
+    $range = $WorkSheet.UsedRange; $range.borderAround(1,3,1); $range.EntireColumn.AutoFit();
+}
+
+# Create a new Excel
+$Excel = New-Object -ComObject excel.application
+$WorkBook = $Excel.Workbooks.add()
+$WorkSheet = $WorkBook.Sheets.Item(1)
+$WorkSheet.DisplayRightToLeft = $false
+$Excel.Visible = $true
+CreateExcel *> $null
+$CurrentRow = 3
+
+# Get all vCenter to map
+$vcenters = (read-host "Enter all vcenters to map (with a ,) between each name").split(',').Replace(" ", "")
+
+# Map each vcenter
+foreach ($vcenter in $vcenters)
+{
+    # Connect to vCenter- works best if the user running this script has permissions on the vCenter
+    Connect-VIServer $vcenter
+    $VMhosts = Get-VMHost | Sort-Object
+    $StartingRow = $CurrentRow
+   
+    # Map all hosts
+    foreach ($VMhost in $VMhosts)
+    {
+        $WorkSheet.Cells.Item($CurrentRow, 1).value() = $vcenter
+        $WorkSheet.Cells.Item($CurrentRow, 2).value() =  (Get-Cluster -VMHost $VMhost).name
+        $WorkSheet.Cells.Item($CurrentRow, 3).value() = $VMhost.name
+        $WorkSheet.Cells.Item($CurrentRow, 4).value() = $vmhost.NumCpu
+        $CurrentRow = $CurrentRow + 1
+    }
+
+    # Auto fit
+    $range = $WorkSheet.UsedRange; $range.borderAround(1,3,1); $range.EntireColumn.AutoFit();
+
+    # Merge all the cells of the vcenter
+    $MergeCells = $WorkSheet.range($WorkSheet.cells.item($StartingRow,1), $WorkSheet.cells.item($CurrentRow - 1,1))
+    $MergeCells.MergeCells = $true; $mergeCells.BorderAround(1,3,1) *> $null ; 
+    $WorkSheet.Cells.item($CurrentRow - 1,1).horizontalalignment = -4108; $WorkSheet.Cells.item($CurrentRow - 1,1).verticalalignment = -4108
+
+    Disconnect-VIServer -Confirm:$false *>$null
+}
---
+++ Map VMS with RDM to their snapmirror.ps1
@@ -0,0 +1,92 @@
+﻿import-module VMware.VimAutomation.Core
+
+#Inputs
+[string]$vcenter = read-host "Please enter vcenter name"
+[string]$folder = read-host "Please enter the folder name of the vms you want to map in"
+[string]$netapp = read-host "Please enter netapp name"
+
+<# If you wish to have a hard coded user to login, use this connection and fill the usr/pass
+$usr = ""
+$pass = ""
+$encryptedPass = New-Object -TypeName System.Security.SecureString
+$pass.ToCharArray() | % {$encryptedPass.AppendChar($_)}
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $usr,$encryptedPass
+Connect-VIServer $vcenter -Credential $cred
+#>
+
+# Connect to vcenter
+"Connecting to vCenter"
+Connect-VIServer $vcenter 
+
+<# If you wish to have a hard coded user to login, use this connection and fill the usr/pass
+$usr = ""
+$pass = ""
+$encryptedPass = New-Object -TypeName System.Security.SecureString
+$pass.ToCharArray() | % {$encryptedPass.AppendChar($_)}
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $usr,$encryptedPass
+Connect-NcController $netapp -Credential $cred -HTTPS
+#>
+
+# Connect to Netapp
+"Connecting to Netapp"
+Connect-NcController $netapp -HTTPS
+
+# Get Vols and Luns
+"Fetching luns and vols"
+$SRCVols = Get-NcVol
+$SRCLuns = Get-NcLunMap
+$MirrorDestinations = Get-NcSnapmirrorDestination 
+
+# Get VMs with RDMs
+"Getting VMs with RDMS"
+$FolderName = Read-Host "Enter Folder Name: "
+$VMsWithRDMs = Get-Folder -name "$FolderName" | Get-VM | ? {(Get-HardDisk $_ -DiskType RawPhysical) -ne $null} | Sort-Object
+$VMsTable = @()
+
+# Map all vms with RDMs
+"Mapping VMs with RDMs"
+foreach ($currVM in $VMsWithRDMs)
+{
+    # Get all RDMs
+    $RDMs = get-harddisk $currVM -DiskType RawPhysical
+    $LunIdList = @()
+
+    # Map each RDM to the lun id
+    foreach ($CurrRDM in $RDMs)
+    {
+        $esxcli = Get-EsxCli -VMHost $currVM.VMHost -v2
+        $LunIdList += $esxcli.storage.nmp.path.list.Invoke(@{'device'=$CurrRDM.ScsiCanonicalName}).RuntimeName.Split(':')[-1].TrimStart('L')
+    }
+
+    $VMsTable += $currVM | select @{name="VM"; Expression={$currVM.Name}}, 
+                                  @{name="Cluster"; Expression={$currVM.VMHost.Parent.name.ToLower()}}, 
+                                  @{Name="LunIDs"; Expression={$LunIdList}}
+}
+
+# Check each vm in the table
+"Checking For Snapmirrors"
+$ResultTable = @()
+foreach ($currRow in $VMsTable)
+{
+    # Find the matching volumes for each of the vm luns
+    foreach ($currLunID in $currRow.LunIDs)
+    {
+        $VolName = ($SRCLuns | ? {($_.InitiatorGroup -eq $currRow.Cluster) -and ($_.LunId -eq $currLunID)}).Path.ToString().Split("/")[2]
+        $CurrVol = $SRCVols | ? {$_.name -eq $volname}
+        $hasMirror = "No"
+        if ($MirrorDestinations | ? {$_.SourceVolume -eq $currvol.name})
+        {
+            $hasMirror = "Yes"
+        }
+
+        $ResultTable += $CurrVol | select @{name="VM"; Expression={$currRow.VM}}, 
+                                          @{name="Cluster"; Expression={$currRow.Cluster}},
+                                          @{Name="Vol Name"; Expression={$VolName}},
+                                          @{Name="LunIDs"; Expression={$currLunID}},
+                                          @{Name="Has Mirror"; Expression={$hasMirror}}
+
+    }
+}
+
+Disconnect-VIServer * -Force:$true -Confirm:$false
+$resulttable | Export-Csv -LiteralPath c:\temp\SnapmirrorCheck\$CurrSite.csv -Force:$true
